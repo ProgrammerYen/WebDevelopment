4c891c1663bd19f05d7a74bcff38201ef2f0ab29
Kaushallya <kaushallyadealwis@yahoo.co.uk>
2022-05-07 23:23:05 +0100

    Introductions to javascript numbesr

---
+++ javascript/number_data_type.js
@@ -0,0 +1,13 @@
+// Arithmetic operations
+console.log(12 + 8); // => 20
+console.log(12 / 2); // => 6
+console.log(6 % 4); // => 2
+console.log((4 + 5) / 2 * 3); // => 13.5
+console.log(9 ** 0.5)
+
+// Negative numbers
+console.log(-2 + 4 / -3); // -3.333...
+
+// Strings 
+console.log("1 + 1"); // => 1 + 1
+console.log("Hello there!"); // => Hello there!
