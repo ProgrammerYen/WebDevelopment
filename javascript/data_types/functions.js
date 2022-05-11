5fda7e7620bf8b0e2ca15830eedf2e1faa7844ca
Kaushallya <kaushallyadealwis@yahoo.co.uk>
2022-05-11 22:36:36 +0100

    Made small changes and adjustments

--- javascript/data_types/functions.js
+++ javascript/data_types/functions.js
@@ -1,7 +1,8 @@
 function average(number1, number2) {
     // Returns the average of two values.
     return (number1 + number2) / 2;
-}
+};
+
 const average_result = average(13.5, 19.7);
 console.log(average_result);
 
@@ -13,7 +14,7 @@ function solveQuadratic(a, b, c) {
 
     result2 = (-b - (b**2 - 4 * a * c) ** 0.5) / 2 * a;
     console.log("Second solution: " + result2);
-
-}
+    return 
+};
 
 solveQuadratic(1, 0, -5);
