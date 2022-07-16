bb26d7f8317fefa2b6c3cc8db40094e4627bff36
Kaushallya <kaushallyadealwis@yahoo.co.uk>
2022-07-16 15:59:46 +0100

    Made changes

--- javascript/experimenting.js
+++ javascript/experimenting.js
@@ -1,35 +1,35 @@
-function rangeOfValues(min, max) {
-    for (let i = min; i < max + 1; i++) {
-        console.log("Number: " + i);
+function displayRange(min, max) {
+    // displays a range of values from min value to max value
+    for (let i=min; i<max+1; i++) {
+        console.log(i);
     }
 }
+displayRange(4, 10);
 
-rangeOfValues(4, 15);
-
-console.log("\n")
-
-function rangeOfValues2(min, max) {
-    while (min < max + 1) {
-        console.log("Number: " + min);
-        min ++;
-    }
-}
-
-rangeOfValues(8,19);
-console.log();
-
-function rollerCoaster(age, height) {
-    if (age < 12) {
-        console.log("I'm sorry but you are too young to ride the rollercoaster");
+function bmiCalculator(height, weight) {
+    // calculates the body mass index of a person using the following equation -> bmi = weight (in kg) / height (in m) ^ 2.
+    if (height > 2.72) {
+        console.log("That is impossible. You cannot be that tall since the tallest person to exist was 2.72 metres.");
     } else {
-        if (height < 150) {
-            console.log("I'm sorry but you are too short to ride this rollercoaster");
-        } else if (height <= 190) {
-            console.log("Get ready to ride the rollercoaster!");
+        console.log("You are a valid candidate.\n");
+        let bmi = (weight / height ** 2).toString();
+        console.log("Your body mass index is ".concat(bmi));
+        if (bmi > 40) {
+            console.log("You are in the severely obese range (40 or over)");
+        } else if (bmi > 30) {
+            console.log("You are in the obese range (30 to 39.9)");
+        } else if (bmi > 25) {
+            console.log("You are in the overweight range (25 to 29.9)");
+        } else if (bmi > 18) {
+            console.log("You are in the healthy range (18 to 25)");
+        } else if (bmi > 16) {
+            console.log("You are in the underweight range (16 to 18)");
+        } else if (bmi > 15) {
+            console.log("You are in the severely underweight range (15 to 16)");
         } else {
-            console.log("I'm sorry but you are too tall to ride this rollercoaster");
+            console.log("You are in the very severely underweight range (15 or below)");
         }
     }
-}
 
-rollerCoaster(14,156);
+}
+bmiCalculator(1.53, 35);
